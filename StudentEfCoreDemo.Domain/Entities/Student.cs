8f7e829f08718feb8183a32a0f8c24368846469e
Zigdiz <treijonen@live.fi>
2025-03-25 16:44:37 +0200

    mediatr example

---
+++ StudentEfCoreDemo.Domain/Entities/Student.cs
@@ -0,0 +1,10 @@
+namespace StudentEfCoreDemo.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+} 
